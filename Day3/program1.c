fc002aca3ef0771e0d55c2092224c481840891eb
Mayank Srivastava <srivastavamayank133@gmail.com>
2020-01-22 14:41:52 +0530

    Day3

---
+++ Day3/program1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	char s[]="Hello	world";
+	int i=0,j=0;
+	char *t=(char*)malloc(sizeof(s));
+	while(s[i] !='\0')
+	{
+		if(s[i] == '\t')
+		{
+			t[j++]='\\';
+			t[j++]='t';
+		}
+		else if(s[i] == '\n')
+		{
+			t[j++] = '\\';
+			t[j++]='n';
+		}
+		else
+		{
+			t[j++]=s[i];
+		}
+		i++;
+	}
+	printf("%s\n",t);
+}
---
+++ Day3/try4.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(void)
+{
+	printf("my name is \r Hello\n");
+	return 0;
+}
