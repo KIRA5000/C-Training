f459a5f0039b31462604402a6b5ff173cb678c7b
Mayank Srivastava <srivastavamayank133@gmail.com>
2020-02-03 12:53:49 +0530

    Day11

---
+++ Day11/dynstr.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+	char *str=(char *)malloc(2);
+	int i=0;
+	int t=2;
+	char s;
+	printf("Enter a string\n");
+	scanf("%c",&s);
+	int check=0;
+	while(check!=10)
+	{
+		str=(char *)realloc(str,(t+1));
+		str[i++]=s;
+		scanf("%c",&s);
+		t++;
+		check=s;
+	}
+	printf("Your string size is:%ld\n",strlen(str));
+	printf("Your string is:%s\n",str);
+	return 0;
+}
