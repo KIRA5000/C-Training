3452b08c6ee7bd2d31f664b44cf75e887e6cca3f
Mayank Srivastava <srivastavamayank133@gmail.com>
2020-02-05 18:10:04 +0530

    Day13

--- Day13/max_min.c
+++ Day13/max_min.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<limits.h>
 int main()
 {
         char *str=(char *)malloc(1);
@@ -9,22 +10,32 @@ int main()
         int choice=s;
         int t=1;
         int i=0;
-	int i1=1;
-	int loc=-1;
-	int *count=(int *)malloc(sizeof(int));
         while(choice!=10)
         {
                 str=(char*)realloc(str,t+1);
                 t++;
                 str[i++]=s;
                 scanf("%c",&s);
-		if(s==' ')
-		{
-
-		}
                 choice=s;
         }
         t--;
-	for(int 
+	int min=INT_MAX;
+	int max=INT_MIN;
+	int count=0;
+	for(int j=0;j<=t;j++)
+	{
+		count++;
+		if(str[j]==' ')
+		{
+			count--;
+			if(min>count)
+				min=count;
+			if(max<count)
+				max=count;
+			count=0;
+		}
+	}
+	printf("Max length:%i\n",max);
+	printf("Min length:%i\n",min);
 	return 0;
 }
