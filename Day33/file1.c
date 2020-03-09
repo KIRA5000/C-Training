b4ee5e91fd115ba8b1622b4d0b3ae7b07f083a4f
Mayank Srivastava <srivastavamayank133@gmail.com>
2020-03-09 10:21:21 +0530

    Day33

---
+++ Day33/file1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(int argc,char **argv)
+{
+	if(argc!=2)
+	{
+		printf("./a.out pathname\n");
+	}
+	else
+	{
+		FILE *fp;
+		if((fp=fopen(argv[1],"a+"))!=NULL)
+		{
+			printf("Enter some data\n");
+			char n;
+			while((n=getchar())!=EOF)
+			{
+				fputc(n,fp);
+			}
+			printf("Your data in file is:\n");
+			rewind(fp);
+			while((n=fgetc(fp))!=EOF)
+			{
+				printf("%c",n);
+			}
+		}
+		else
+		{
+			ferror(fp);
+		}
+	}
+	
+	return 0;
+}
